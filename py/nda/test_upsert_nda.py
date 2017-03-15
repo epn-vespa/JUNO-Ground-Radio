dbba75335b0af498e352cd63bbc1c0c3b554b20f
Baptiste Cecconi <baptiste.cecconi@obspm.fr>
2017-03-15 16:41:32 +0100

    update upsert_nda

---
+++ py/nda/test_upsert_nda.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+import os
+import glob
+from spacepy import pycdf
+
+data_dir = "/databf/nda/web/jupiter/data/"
+plot_dir = "/databf/nda/web/jupiter/ql/B-W/"
+data_yr = "2017"
+data_mo = "01"
+cdf_list = sorted(glob.glob(os.path.join(data_dir, data_yr, data_mo, "*.cdf")))
+pdf_list = sorted(glob.glob(os.path.join(plot_dir, data_yr, data_mo, "J??????.pdf")))
+ncdf = len(cdf_list)
+npdf = len(pdf_list)
+
+if ncdf != npdf:
+    print 'error !'
+else:
+    for i in range(ncdf):
+        os.system('python upsert_nda.py {} {}'.format(cdf_list[i], pdf_list[i]))
--- py/nda/upsert_nda.py
+++ py/nda/upsert_nda.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
 from spacepy import pycdf
-from astropy.time import Time
 import os
 import platform
 import sys
@@ -8,6 +7,7 @@ import psycopg2
 import datetime
 import hashlib
 import json
+
 
 def init_md():
     """
@@ -19,6 +19,7 @@ def init_md():
     md['len'] = 0
     md['tab'] = []
     return md
+
 
 def md5sum(filename, blocksize=65536):
     """
@@ -32,7 +33,8 @@ def md5sum(filename, blocksize=65536):
         for block in iter(lambda: fhdl.read(blocksize), b""):
             md5hash.update(block)
     return md5hash.hexdigest()
-    
+
+
 def creation_date(path_to_file):
     """
     Try to get the date that a file was created, falling back to when it was
@@ -47,13 +49,14 @@ def creation_date(path_to_file):
     else:
         stat = os.stat(path_to_file)
         try:
-            t=  stat.st_birthtime
+            t = stat.st_birthtime
         except AttributeError:
             # We're probably on Linux. No easy way to get creation dates here,
             # so we'll settle for when its content was last modified.
             t = stat.st_mtime
     return datetime.datetime.fromtimestamp(t).isoformat()
-    
+
+
 def modification_date(filename):
     """
     extract modification date
@@ -63,7 +66,8 @@ def modification_date(filename):
 
     t = os.path.getmtime(filename)
     return datetime.datetime.fromtimestamp(t).isoformat()
-    
+
+
 def access_url(file_name, parent_name, file_type):
     """
     assemble access URL
@@ -75,9 +79,9 @@ def access_url(file_name, parent_name, file_type):
     
     yy = int(parent_name[1:3])
     if yy > 70:
-        yy +=1900
+        yy += 1900
     else: 
-        yy +=2000
+        yy += 2000
     mm = int(parent_name[3:5])
     if file_type == 'cdf':
         root_url = 'http://realtime.obs-nancay.fr/routine_jupiter'
@@ -95,92 +99,98 @@ def access_url(file_name, parent_name, file_type):
         print 'Illegal file type... Aborting'
         return '' 
 
-def nda_metadata(file_cdf, file_pdf, type):
+
+def nda_metadata(file_cdf, file_pdf, file_type):
     """
-    extract metadata from CDF
+    extract metadata from CDF and build metadata item for provided file type
     :param file_cdf: input CDF file path
     :param file_pdf: input PDF file path
+    :param file_type: file type
     :return metadata: metadata for file
     :return status: 0 if success; -1 if error
     """
     
     cdf = pycdf.CDF(file_cdf)
-    metadata = {}
-    if type == 'cdf':
+    md = dict()
+    if file_type == 'cdf':
         cur_file = file_cdf
-        metadata['granule_uid'] = cdf.attrs['Parents'][0].split('.')[0]+'_cdf'
-        metadata['granule_gid'] = 'SRN NDA EDR Routine Data'
-        metadata['creation_date'] = creation_date(cur_file)
-        metadata['access_format'] = cdf.attrs['VESPA_access_format'][0]
-        metadata['processing_level'] = 2
-    elif type == 'pdf':
+        md['granule_uid'] = cdf.attrs['Parents'][0].split('.')[0]+'_cdf'
+        md['granule_gid'] = 'SRN NDA EDR Routine Data'
+        md['creation_date'] = creation_date(cur_file)
+        md['access_format'] = cdf.attrs['VESPA_access_format'][0]
+        md['processing_level'] = 2
+    elif file_type == 'pdf':
         cur_file = file_pdf
-        metadata['granule_uid'] = cdf.attrs['Parents'][0].split('.')[0]+'_pdf'
-        metadata['granule_gid'] = 'SRN NDA PDF Routine Summary Plots'
-        metadata['creation_date'] = cdf.attrs['PDS_Observation_stop_time'][0]
-        metadata['access_format'] = 'application/pdf'
-        metadata['processing_level'] = 4
-    elif type == 'rt1':
+        md['granule_uid'] = cdf.attrs['Parents'][0].split('.')[0]+'_pdf'
+        md['granule_gid'] = 'SRN NDA PDF Routine Summary Plots'
+        md['creation_date'] = cdf.attrs['PDS_Observation_stop_time'][0]
+        md['access_format'] = 'application/pdf'
+        md['processing_level'] = 4
+    elif file_type == 'rt1':
         cur_file = os.path.join(os.path.dirname(file_cdf), cdf.attrs['Parents'][0])
-        metadata['granule_uid'] = cdf.attrs['Parents'][0].split('.')[0]
-        metadata['granule_gid'] = 'SRN NDA RT1 Routine Raw Data'
-        metadata['creation_date'] = cdf.attrs['PDS_Observation_stop_time'][0]
-        metadata['access_format'] = 'application/binary'
-        metadata['processing_level'] = 1
+        md['granule_uid'] = cdf.attrs['Parents'][0].split('.')[0]
+        md['granule_gid'] = 'SRN NDA RT1 Routine Raw Data'
+        md['creation_date'] = cdf.attrs['PDS_Observation_stop_time'][0]
+        md['access_format'] = 'application/binary'
+        md['processing_level'] = 1
     else:
         print 'Illegal file type... Aborting'
-        return metadata,-1
-        
-    metadata['obs_id'] = cdf.attrs['Parents'][0].split('.')[0]
-    metadata['target_name'] = '#'.join(cdf.attrs['PDS_Observation_target'])
-    metadata['target_class'] = '#'.join(cdf.attrs['VESPA_target_class'])
-    metadata['time_min'] = float(Time(cdf.attrs['PDS_Observation_start_time']).jd[0])
-    metadata['time_max'] = float(Time(cdf.attrs['PDS_Observation_stop_time']).jd[0])
-    metadata['time_sampling_step'] = float(cdf.attrs['VESPA_time_sampling_step'][0])
-    metadata['time_exp'] = float(cdf.attrs['VESPA_time_exp'][0])
-    metadata['spectral_range_min'] = float(cdf.attrs['VESPA_spectral_range_min'][0])
-    metadata['spectral_range_max'] = float(cdf.attrs['VESPA_spectral_range_max'][0])
-    metadata['spectral_sampling_step'] = float(cdf.attrs['VESPA_spectral_sampling_step'][0])
-    metadata['spectral_resolution'] = float(cdf.attrs['VESPA_spectral_resolution'][0])
-    metadata['measurement_type'] = '#'.join(cdf.attrs['VESPA_measurement_type'])
-    metadata['receiver_name'] = cdf.attrs['VESPA_receiver_name'][0].split('>')[0]
-    metadata['modification_date'] = modification_date(cur_file)
-    metadata['release_date'] = datetime.datetime.now().isoformat()
-    metadata['access_md5'] = md5sum(cur_file)
-    metadata['thumbnail_url'] = access_url(os.path.basename(file_cdf), data['obs_id'], 'png')
-    metadata['target_region'] = '#'.join(cdf.attrs['VESPA_target_region'])
-    metadata['feature_name'] = '#'.join(cdf.attrs['VESPA_feature_name'])
-    metadata['bib_reference'] = '#'.join(cdf.attrs['VESPA_bib_reference'])
-    metadata['access_url'] = access_url(os.path.basename(file_cdf), data['obs_id'], type)
-    metadata['file_name'] = os.path.basename(cur_file)
-    metadata['access_estsize'] = os.path.getsize(cur_file)/1024
+        return md, -1
+
+    md['obs_id'] = cdf.attrs['Parents'][0].split('.')[0]
+    md['target_name'] = '#'.join(cdf.attrs['PDS_Observation_target'])
+    md['target_class'] = '#'.join(cdf.attrs['VESPA_target_class'])
+    md['time_min'] = float(Time(cdf.attrs['PDS_Observation_start_time']).jd[0])
+    md['time_max'] = float(Time(cdf.attrs['PDS_Observation_stop_time']).jd[0])
+    md['time_sampling_step'] = float(cdf.attrs['VESPA_time_sampling_step'][0])
+    md['time_exp'] = float(cdf.attrs['VESPA_time_exp'][0])
+    md['spectral_range_min'] = float(cdf.attrs['VESPA_spectral_range_min'][0])
+    md['spectral_range_max'] = float(cdf.attrs['VESPA_spectral_range_max'][0])
+    md['spectral_sampling_step'] = float(cdf.attrs['VESPA_spectral_sampling_step'][0])
+    md['spectral_resolution'] = float(cdf.attrs['VESPA_spectral_resolution'][0])
+    md['measurement_type'] = '#'.join(cdf.attrs['VESPA_measurement_type'])
+    md['receiver_name'] = cdf.attrs['VESPA_receiver_name'][0].split('>')[0]
+    md['modification_date'] = modification_date(cur_file)
+    md['release_date'] = datetime.datetime.now().isoformat()
+    md['access_md5'] = md5sum(cur_file)
+    md['thumbnail_url'] = access_url(os.path.basename(file_cdf), data['obs_id'], 'png')
+    md['target_region'] = '#'.join(cdf.attrs['VESPA_target_region'])
+    md['feature_name'] = '#'.join(cdf.attrs['VESPA_feature_name'])
+    md['bib_reference'] = '#'.join(cdf.attrs['VESPA_bib_reference'])
+    md['access_url'] = access_url(os.path.basename(file_cdf), data['obs_id'], file_type)
+    md['file_name'] = os.path.basename(cur_file)
+    md['access_estsize'] = os.path.getsize(cur_file)/1024
     cdf.close()
 
-    return metadata,0
+    return md, 0
 
-def ingest(md, cur):
+
+def ingest(md, db_cur):
     """
     ingest metadata into nda.metadata
     :param md: metadata of file
-    :param cur: current database access handle
+    :param db_cur: current database access handle
     """
     
     for item in md['tab']:
-        cur.execute("select count(*) from nda.metadata where granule_uid = {}".format(item['granule_uid']) # test to avoid multiple same entry
-        number_of_rows = cur.fetchone()[0]
+
+        # test to avoid multiple same entry
+        db_cur.execute("select count(*) from nda.metadata where granule_uid = {}".format(item['granule_uid']))
+
+        number_of_rows = db_cur.fetchone()[0]
         if number_of_rows == 1:
             query = "UPDATE nda.metadata SET " 
             first = True
             for k in item.keys(): 
                 if first:
-                    query += "{}={}".format(k,'%s')
+                    query += "{}={}".format(k, '%s')
                     param = (item[k],)
                     first = False
                 else:
-                    query += ", {}={}".format(k,'%s')
-                    param = param + (item[k],)
+                    query += ", {}={}".format(k, '%s')
+                    param = (param, item[k])
             query += " WHERE granule_uid = %s;"
-            param = param + (item['granule_uid'],)
+            param = (param, item['granule_uid'])
             
         else:
             query = "INSERT INTO nda.metadata "  
@@ -196,20 +206,21 @@ def ingest(md, cur):
                 else:
                     query_key += ", {}".format(k)
                     query_val += ", %s"
-                    param = param + (item[k],)
+                    param = (param, item[k])
             query_key += ")"
             query_val += ")"
-            query += """{} VALUES {};""".format(query_key,query_val) 
+            query += """{} VALUES {};""".format(query_key, query_val)
 
-        print cur.mogrify(query, param)
-        cur.execute(query, param)
+        print db_cur.mogrify(query, param)
+        db_cur.execute(query, param)
 
     return
 
-file_cdf = sys.argv[1]
-file_pdf = sys.argv[2]
 
-if file_cdf == '--help':
+fcdf = sys.argv[1]
+fpdf = sys.argv[2]
+
+if fcdf == '--help':
     print "NAME"
     print "    upsert_nda.py  -- update or insert nda record in nda.metadata table"
     print "SYNOPSIS"
@@ -221,11 +232,11 @@ if file_cdf == '--help':
     print "DEPENDENCIES"
     print "    The following python libraries must be installed: spacepy, astropy, psycopg2, datetime, hashlib, json"
 
-metadata=init_md()
+metadata = init_md()
 file_types = ['cdf', 'pdf', 'rt1']
 
-for type in file_types:
-    data,status = nda_metadata(file_cdf, file_pdf, type)
+for ftype in file_types:
+    data, status = nda_metadata(fcdf, fpdf, ftype)
     if status == 0:
         metadata['tab'].append(data)
         metadata['len'] += 1
@@ -233,16 +244,16 @@ for type in file_types:
 # print metadata
 
 # copy the file credential.json to credential_vogate.json and insert gavoadmin password in file.
-with open('credential_vogate.json') as f:
-    mydb = json.load(f)
+with open('config.json') as f:
+    config = json.load(f)
 
-mydatabase = mydb['database']
-myhost = mydb['host']
-myuser = mydb['user']
-mypasswd = mydb['passwd']
+mydatabase = config['db']['database']
+myhost = config['db']['host']
+myuser = config['db']['user']
+mypasswd = config['db']['passwd']
 
 try:
-    con = psycopg2.connect(database= mydatabase, user= myuser, host= myhost, password= mypasswd, port='5432')
+    con = psycopg2.connect(database=mydatabase, user=myuser, host=myhost, password=mypasswd, port='5432')
     cur = con.cursor()
 
 except psycopg2.DatabaseError, e:
@@ -256,5 +267,3 @@ if con:
     con.close()
 else:
     print "Problem to access the database, check the configuration"
-
-
